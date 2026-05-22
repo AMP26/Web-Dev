734b7243731c538b502b661d8a2126df395c89e3
AMP26 <amit.patnaik0126@gmail.com>
2026-05-22 13:20:52 +0530

    express-server

--- express-js/index.js
+++ express-js/index.js
@@ -2,7 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.send('<h1>Hello</h1>')
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running on PORT: ${PORT}`);
-})
+});
